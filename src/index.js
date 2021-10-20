406077dde484864075d86f567c04c334fc2273de
imsikun <imsikun16@gmail.com>
2021-10-20 17:08:44 +0530

    application complete

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ResultContextProvider } from './contexts/ResultContextProvider'
 
 import './global.css'
 
 import App from './App'
 
 ReactDom.render(
-  <Router>
-  <App /> 
-  </Router>,
+  <ResultContextProvider>
+   <Router>
+      <App /> 
+   </Router>
+  </ResultContextProvider>,
 document.getElementById('root')
 )
